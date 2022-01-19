718521ca607f5fe92ab95c163539f69bef15d4cd
Juan Lucero <jlucero@alhpaworks.tech>
2022-01-19 09:56:13 -0800

    feat(content): refactor content

---
+++ src/Components/CareerDevelopment.js
@@ -0,0 +1,54 @@
+
+export default function CareerDevelopment() {
+
+  return(
+    <div>
+      <h1>Career Development</h1>
+  
+      <h2>CONTENT.ad – IRVINE, CA: 2015-2021
+        Software Engineer (Front End &amp; WordPress)</h2>
+
+      <p>Content.ad is a fast paced digital ad provider. They build, manage and maintain a network of advertisers and publishers that serve massive amounts of digital ads every hour.</p>
+
+      <ul>
+        <li>Assisted with project management</li>
+        <li>React site development for internal users and Advertiser/Publishers in the ad network</li>
+        <li>Maintain &amp; extend our Ad Widget UI elements using HTML, CSS, &amp; vanilla JavaScript to layout &amp; animate our ad display containers</li>
+        <li>Develop &amp; maintain our easy to use Content.ad WordPress Plugin</li>
+        <li>Maintain several high traffic WordPress sites for our partners and clients including the Cotnent.ad Marketing site at www.content.ad.</li>
+      </ul>
+
+      <h2>Experian Consumer Direct | Costa Mesa, CA: 2012 – 2015 | Front End Web Developer</h2>
+
+      <p>I worked on white label builds of Experian's FreeCreditReport.com and FreeCreditScore.com sites for corporate clients. These custom builds are used to provide a vast array of credit report data and monitoring services to end users through their robust web services.</p>
+
+      <ul>
+        <li>Web Accessibility Expert for one of Experian's biggest clients, Wells Fargo</li>
+        <li>I utilized tools such as Visual Studio with git, and the Tridion CMS among others.</li>
+        <li>Bootstrap Expert for an intensive 3 month build of USAA's angular Credit Check products.</li>
+      </ul>
+
+      <h2>Yamaha | Buena Park, CA: 2009 – 2012 | Front End Web Developer Lead</h2>
+
+      <p>As Yamaha's HTML/CSS expert, I developed feature/promotion websites using JavaScript (jQuery) that included: time based pre-recorded &amp; live video, and slideshow galleries. I aided in the redesign/migration of Yamaha Corporation of America usa.yamaha.com. Coordinated/contributed to product and feature website development with an overseas team in India. I communicated directly with clients to clarify questions and respond directly to time sensitive product and feature site issues.</p>
+
+      <h2>e:Design, inc. | Fresno, CA: 2007 – 2009 | Web Architect</h2>
+
+      <p>I worked in a tight knit team consisting of designers, programmers and sales people on print and web projects for education, business and non-profit organizations; both local to the Fresno area and across the US. We utilized several CMS' to create attractive interactive experiences using HTML/CSS/JS (jQuery) to achieve complex visual design goals. Utilized screen sharing software to train clients on the use of their new web sites. Assisted with the debugging, maintenance and continued development of existing online applications in PHP/MySQL.
+      </p>
+
+      <h2>NetPas, inc. | Fresno, CA: 2005 – 2007 | Web Designer</h2>
+
+      <p>Sole web developer for company and customer web development. I worked extensively on the internal CMS written in ASP/SQL including case management, customer management, and reporting modules. I worked with clients directly and through the sales staff to develop their business web sites. Designed numerous websites for local non-profits, higher education, and small businesses. Developed a print marketing campaign with sales staff including concept and graphic design of documents and mailers.</p>
+
+      <h2>Lucero Web Design | Fresno, CA: 2004 – 2005 | Owner</h2>
+
+      <p>I owned &amp; operated a full service web design and maintenance service, providing services to non-profit, higher education and small businesses. I managed consultants and made strategic decisions on the financial health of the business. I continued to work extensively with CSU, Fresno on projects that directly affected the student experience, including a PHP/MySQL phone directory and class requirements site.</p>
+
+      <h2>CSU, Fresno | Fresno, CA: 2001 – 2004 | Assistant Web Manager</h2>
+
+      <p>I performed design, management and maintenance tasks integral to the university's web presence including: work on the university's web accessibility guidelines and implementation, university top pages, Fresno State News, The University Journal, Phone Directory, website index (PHP/MySQL), and redirected webmaster email inquiries. I also trained students, faculty, and administrators on university policy, site templates, and design guidelines. </p>
+
+    </div>
+  )
+}
---
+++ src/Components/Proficiencies.js
@@ -0,0 +1,25 @@
+
+export default function Proficiencies() {
+
+  return(
+    <div>
+      <h1>Proficiencies</h1>
+
+      <ul className="text-columns">
+        <li>Agile/scrum master</li>
+        <li>git/GitHub/Bitbucket</li>
+        <li>Mobile cross browser optimization</li>
+        <li>HTML5/CSS3/LESS/SASS</li>
+        <li>React/JS/Node/npm/yarn</li>
+        <li>Mobile/Web Responsive Layouts</li>
+        <li>Web Accessibility</li>
+        <li>PHP/MySQL/WordPress</li>
+        <li>VS Code/Visual Studio</li>
+        <li>Bootstrap Rapid Prototyping</li>
+        <li>Windows/Mac/iOS/Android</li>
+        <li>ASP/SQL (for .NET shops)</li>
+      </ul>
+
+    </div>
+  )
+}
