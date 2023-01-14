4034ba5dce3ab3efc4d812216f1c6bf20ea33cce
Juan Lucero <luceroweb@gmail.com>
2023-01-14 11:39:18 -0800

    chore(react-app): crate new react app from scratch

--- src/App.js
+++ src/App.js
@@ -1,71 +1,23 @@
-import { useState } from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import Nav from './Components/Nav';
-import Footer from './Components/Footer';
-import Home from './Components/Home';
-import CareerDevelopment from './Components/CareerDevelopment';
-import Portfolio from './Components/Portfolio';
-import Proficiencies from './Components/Proficiencies';
-import Education from './Components/Education';
-import Contact from './Components/Contact';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('Profile');
-
   return (
     <div className="App">
-      <HashRouter>
-        <div className="container">
-
-          <div className="row g-lg-0">
-
-            <div className="col-lg-4">
-              
-              <Nav
-                currentPage={currentPage}
-                setCurrentPage={setCurrentPage}
-              />
-
-            </div>
-
-            <div className="col-lg-8">
-
-              <div className="bg-white m-3 p-3 border rounded-3 clearfix">
-
-                  <Routes>
-                    <Route exact activeClassName="active" path='/' element={
-                      <Home />
-                    } />
-                    <Route exact activeClassName="active" path='/career-development' element={
-                      <CareerDevelopment />
-                    } />
-                    <Route exact activeClassName="active" path='/portfolio' element={
-                      <Portfolio />
-                    } />
-                    <Route exact activeClassName="active" path='/proficiencies' element={
-                      <Proficiencies />
-                    } />
-                    <Route exact activeClassName="active" path='/education' element={
-                      <Education />
-                    } />
-                    <Route exact activeClassName="active" path='/contact' element={
-                      <Contact />
-                    } />
-                  </Routes>
-
-              </div>
-
-            </div>
-          </div>
-
-          <Footer
-            setCurrentPage={setCurrentPage}
-          />
-
-        </div>
-      </HashRouter>
-
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
