a4e2f14a018c30cdc3575f8e64df7bde31ca99ca
Juan Lucero <luceroweb@gmail.com>
2023-01-14 13:37:20 -0800

    feat(layout): add basic layout & files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import face from "./images/face.jpg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <strong>Juan</strong> Lucero | <small>SENIOR WEB DEVELOPER</small>
       </header>
+      <main>
+        <div>
+          <h1>22 years moving user focused businesses forward</h1>
+          <p>
+            For the past 22 years, I've enjoyed building collaborative team
+            projects that put your users first. If your user thinks “what's this
+            for?” then the job isn't done yet. Happy users come back to your
+            business again and again; happy teams work harder, faster, and take
+            pride in their work. Invite me to do this for your business.
+          </p>
+        </div>
+        <div>
+          <img src={face} alt="" />
+        </div>
+      </main>
     </div>
   );
 }
