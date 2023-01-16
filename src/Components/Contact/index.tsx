08f262ce78fedfba598e7830d80fb6e85543f8cb
Juan Lucero <luceroweb@gmail.com>
2023-01-16 15:35:51 -0800

    feat(contact): add contact form

--- src/Components/Contact/index.tsx
+++ src/Components/Contact/index.tsx
@@ -1,14 +1,53 @@
-import React from "react";
-import { Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
 
-const Header = () => {
+const Contact = () => {
+  const [message, setMessage] = useState<string>("");
+  const [isSent, setIsSent] = useState<boolean>(false);
+
+  const sendContactForm = () => {
+    if (!message.trim()) {
+      return;
+    }
+
+    let mailto = `mailto:luceroweb@gmail.com`;
+    mailto += `?subject=Juan Lucero's Portfolio`;
+    mailto += `&body=${message}`;
+    window.open(mailto);
+    setIsSent(true);
+  };
+
   return (
     <Row>
-      <Col sm={6} md={8}>
+      <Col>
         <h1 className="fs-5">Contact</h1>
+
+        {isSent ? (
+          <h1 className="fs-3">
+            <strong>Thanks for contacting me</strong>
+          </h1>
+        ) : (
+          <Form>
+            <Form.Group className="mb-3" controlId="message">
+              <Form.Label>Message</Form.Label>
+              <Form.Control
+                as="textarea"
+                onChange={(event) => setMessage(event.target.value)}
+                placeholder="Write your message here..."
+                value={message}
+                rows={3}
+                required
+              />
+            </Form.Group>
+
+            <Button variant="primary" onClick={sendContactForm} type="submit">
+              Send
+            </Button>
+          </Form>
+        )}
       </Col>
     </Row>
   );
 };
 
-export default Header;
+export default Contact;
