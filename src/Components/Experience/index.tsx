73b173874df00527e799b224fa2bf53b349a05eb
Juan Lucero <jlucero@alhpaworks.tech>
2023-02-03 09:45:10 -0800

    feat(style): add timeline UI to Experience page

--- src/Components/Experience/index.tsx
+++ src/Components/Experience/index.tsx
@@ -3,201 +3,257 @@ import { Row, Col } from "react-bootstrap";
 
 const Experience = () => {
   return (
-    <Row>
-      <Col>
-        <h1 className="fs-4">Experience AKA Career Development</h1>
-        <h2 className="fs-5">
-          AlphaWorks/Bitwise | <strong>Apprentice Lead (Manager)</strong>
-        </h2>
-        <p>
-          <small>Fresno, CA: 2021 – 2023</small>
-        </p>
-        <p>React/React-Native lead for React MVPs and Caffeine Overflow.</p>
-        <ul>
-          <li>
-            <strong>React & Native w/ TypeScript</strong> collaborative project{" "}
-            <strong>hands on training</strong>
-          </li>
-          <li>
-            Provided <strong>remote instruction</strong> focusing on{" "}
-            <strong>collaborative</strong> team building
-          </li>
-          <li>
-            Trained:{" "}
-            <strong>HTML/CSS/JS, Agile Methodology, gitHub, debugging</strong>
-          </li>
-        </ul>
-        <h2 className="fs-5">
-          Content.ad | <strong>Front End Web Developer</strong>
-        </h2>
-        <p>
-          <small>Irvine, CA: 2015 – 2021</small>
-        </p>
-        <p>
-          Helped build/maintain digital ad network serving 1 billion+
-          impressions/day.
-        </p>
-        <ul>
-          <li>
-            <strong>Scrum Master</strong> for company Portal.
-          </li>
-          <li>
-            <strong>React development</strong> for internal users, advertisers,
-            and publishers
-          </li>
-          <li>
-            Maintained & extended <strong>HTML/CSS/JS</strong> ad layouts &
-            animations
-          </li>
-          <li>
-            Maintained high traffic WP sites plus primary marketing site & WP
-            Plugin
-          </li>
-        </ul>
-        <h2 className="fs-5">
-          Experian Consumer Direct | <strong>Front End Web Developer</strong>
-        </h2>
-        <p>
-          <small>Costa Mesa, CA: 2012 – 2015</small>
-        </p>
-        <p>
-          Helped build{" "}
-          <strong>
-            Experian's{" "}
-            <a
-              href="https://FreeCreditReport.com"
-              rel="noreferrer"
-              target="_blank"
-            >
-              FreeCreditReport.com
-            </a>
-            /
-            <a
-              href="https://FreeCreditScore.com"
-              rel="noreferrer"
-              target="_blank"
-            >
-              FreeCreditScore.com
-            </a>
-          </strong>{" "}
-          white label sites for corporate clients.
-        </p>
-        <ul>
-          <li>
-            <strong>Web Accessibility Expert</strong> for Experian's client,{" "}
-            <strong>Wells Fargo</strong>
-          </li>
-          <li>
-            Utilized <strong>Visual Studio with git</strong>, and Tridion CMS
-            among others.
-          </li>
-          <li>
-            <strong>Bootstrap Expert</strong> for{" "}
-            <strong>USAA's Angular Credit Check products</strong>
-          </li>
-        </ul>
-        <h2 className="fs-5">
-          Yamaha | <strong>Front End Web Developer Lead</strong>
-        </h2>
-        <p>
-          <small>Buena Park, CA: 2009 – 2012.</small>
-        </p>
-        <p>
-          Developed promo sites using <strong>HTML/CSS/jQuery</strong>
-        </p>
-        <ul>
-          <li>
-            Developed live & pre-recorded{" "}
-            <strong>video and slideshow galleries</strong>
-          </li>
-          <li>
-            Aided{" "}
-            <a href="https://usa.yamaha.com" rel="noreferrer" target="_blank">
-              Yamaha Corporation of America
-            </a>{" "}
-            redesign/migration
-          </li>
-          <li>
-            Direct internal client communication to{" "}
-            <strong>manage dev team in India</strong>
-          </li>
-        </ul>
-        <h2 className="fs-5">
-          e:Design, inc. | <strong>Web Architect</strong>
-        </h2>
-        <p>
-          <small>Fresno, CA: 2007 – 2009</small>
-        </p>
-        <p>
-          Tight knit print and web family; building projects for education,
-          business and nonprofit.
-        </p>
-        <ul>
-          <li>
-            Created, debugged, maintained exciting <strong>WordPress</strong>{" "}
-            sites using <strong>PHP/MySQL/HTML/CSS/jQuery</strong>
-          </li>
-          <li>Trained clients remotely on use of WordPress</li>
-        </ul>
-        <h2 className="fs-5">
-          NetPas, inc. | <strong>Web Designer</strong>
-        </h2>
-        <p>
-          <small>Fresno, CA: 2005 – 2007</small>
-        </p>
-        <ul>
-          <li>
-            <strong>Sole web developer</strong> for company and direct client
-            communication: nonprofits, higher education, agriculture and small
-            businesses
-          </li>
-          <li>
-            Extended <strong>ASP/SQL</strong> CMS: reporting, case & customer
-            management
-          </li>
-          <li>
-            <strong>Collaborated with sales on concept/design</strong> of client
-            projects & print
-          </li>
-        </ul>
-        <h2 className="fs-5">
-          Lucero Web Design | <strong>Owner</strong>
-        </h2>
-        <p>
-          <small>Fresno, CA: 2004 – 2005</small>
-        </p>
-        <ul>
-          <li>
-            Owned/operated full service design & maintenance: nonprofit, higher
-            education and small businesses
-          </li>
-          <li>Managed consultants</li>
-          <li>
-            <strong>CSU, Fresno: PHP/MySQL</strong> phone directory, class
-            requirements site
-          </li>
-        </ul>
-        <h2 className="fs-5">
-          CSU, Fresno | <strong>Assistant Web Manager</strong>
-        </h2>
-        <p>
-          <small>Fresno, CA: 2001 – 2004</small>
-        </p>
-        <ul>
-          <li>
-            Design/manage/maintain integral university sites:{" "}
-            <strong>Web accessibility guidelines, primary site</strong>, Fresno
-            State News, University Journal,{" "}
-            <strong>Phone Directory, Site Index</strong>, and directed contact
-            page inquiries
-          </li>
-          <li>
-            Trained students/faculty/administrators on policy, templates, and
-            guidelines
-          </li>
-        </ul>
-      </Col>
-    </Row>
+    <>
+      <Row>
+        <Col>
+          <h1 className="fs-4">Experience AKA Career Development</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="timeline mt-3">
+          <section aria-labelledby="alphaworks">
+            <h2 id="alphaworks">2021 – 2023</h2>
+            <div>
+              <h3>
+                <i className="bi bi-geo-alt-fill"></i> Fresno, CA
+              </h3>
+              <h3>
+                AlphaWorks/Bitwise | <strong>Apprentice Lead (Manager)</strong>
+              </h3>
+              <p>
+                React/React-Native lead for React MVPs and Caffeine Overflow.
+              </p>
+              <ul>
+                <li>
+                  <strong>React & Native w/ TypeScript</strong> collaborative
+                  project <strong>hands on training</strong>
+                </li>
+                <li>
+                  Provided <strong>remote instruction</strong> focusing on{" "}
+                  <strong>collaborative</strong> team building
+                </li>
+                <li>
+                  Trained:{" "}
+                  <strong>
+                    HTML/CSS/JS, Agile Methodology, gitHub, debugging
+                  </strong>
+                </li>
+              </ul>
+            </div>
+          </section>
+          <section aria-labelledby="content-ad">
+            <h2 id="content-ad">2015 – 2021</h2>
+            <div>
+              <h3>
+                <i className="bi bi-geo-alt-fill"></i> Irvine, CA
+              </h3>
+              <h3>
+                Content.ad | <strong>Front End Web Developer</strong>
+              </h3>
+              <p>
+                Helped build/maintain digital ad network serving 1 billion+
+                impressions/day.
+              </p>
+              <ul>
+                <li>
+                  <strong>Scrum Master</strong> for company Portal.
+                </li>
+                <li>
+                  <strong>React development</strong> for internal users,
+                  advertisers, and publishers
+                </li>
+                <li>
+                  Maintained & extended <strong>HTML/CSS/JS</strong> ad layouts
+                  & animations
+                </li>
+                <li>
+                  Maintained high traffic WP sites plus primary marketing site &
+                  WP Plugin
+                </li>
+              </ul>
+            </div>
+          </section>
+          <section aria-labelledby="experian">
+            <h2 id="experian">2012 – 2015</h2>
+            <div>
+              <h3>
+                <i className="bi bi-geo-alt-fill"></i> Costa Mesa, CA:{" "}
+              </h3>
+              <h3>
+                Experian Consumer Direct |{" "}
+                <strong>Front End Web Developer</strong>
+              </h3>
+              <p>
+                Helped build{" "}
+                <strong>
+                  Experian's{" "}
+                  <a
+                    href="https://FreeCreditReport.com"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    FreeCreditReport.com
+                  </a>{" "}
+                  /{" "}
+                  <a
+                    href="https://FreeCreditScore.com"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    FreeCreditScore.com
+                  </a>
+                </strong>{" "}
+                white label sites for corporate clients.
+              </p>
+              <ul>
+                <li>
+                  <strong>Web Accessibility Expert</strong> for Experian's
+                  client, <strong>Wells Fargo</strong>
+                </li>
+                <li>
+                  Utilized <strong>Visual Studio with git</strong>, and Tridion
+                  CMS among others.
+                </li>
+                <li>
+                  <strong>Bootstrap Expert</strong> for{" "}
+                  <strong>USAA's Angular Credit Check products</strong>
+                </li>
+              </ul>
+            </div>
+          </section>
+          <section aria-labelledby="yamaha">
+            <h2 id="yamaha">2009 – 2012</h2>
+            <div>
+              <h3>
+                <i className="bi bi-geo-alt-fill"></i> Buena Park, CA
+              </h3>
+              <h3>
+                Yamaha | <strong>Front End Web Developer Lead</strong>
+              </h3>
+              <p>
+                Developed promo sites using <strong>HTML/CSS/jQuery</strong>
+              </p>
+              <ul>
+                <li>
+                  Developed live & pre-recorded{" "}
+                  <strong>video and slideshow galleries</strong>
+                </li>
+                <li>
+                  Aided{" "}
+                  <a
+                    href="https://usa.yamaha.com"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    Yamaha Corporation of America
+                  </a>{" "}
+                  redesign/migration
+                </li>
+                <li>
+                  Direct internal client communication to{" "}
+                  <strong>manage dev team in India</strong>
+                </li>
+              </ul>
+            </div>
+          </section>
+          <section aria-labelledby="edesign">
+            <h2 id="edesign">2007 – 2009</h2>
+            <div>
+              <h3>
+                <i className="bi bi-geo-alt-fill"></i> Fresno, CA
+              </h3>
+              <h3>
+                e:Design, inc. | <strong>Web Architect</strong>
+              </h3>
+              <p>
+                Tight knit print and web family; building projects for
+                education, business and nonprofit.
+              </p>
+              <ul>
+                <li>
+                  Created, debugged, maintained exciting{" "}
+                  <strong>WordPress</strong> sites using{" "}
+                  <strong>PHP/MySQL/HTML/CSS/jQuery</strong>
+                </li>
+                <li>Trained clients remotely on use of WordPress</li>
+              </ul>
+            </div>
+          </section>
+          <section aria-labelledby="netpas">
+            <h2 id="netpas">2005 – 2007</h2>
+            <div>
+              <h3>
+                <i className="bi bi-geo-alt-fill"></i> Fresno, CA
+              </h3>
+              <h3>
+                NetPas, inc. | <strong>Web Designer</strong>
+              </h3>
+              <ul>
+                <li>
+                  <strong>Sole web developer</strong> for company and direct
+                  client communication: nonprofits, higher education,
+                  agriculture and small businesses
+                </li>
+                <li>
+                  Extended <strong>ASP/SQL</strong> CMS: reporting, case &
+                  customer management
+                </li>
+                <li>
+                  <strong>Collaborated with sales on concept/design</strong> of
+                  client projects & print
+                </li>
+              </ul>
+            </div>
+          </section>
+          <section aria-labelledby="lucerowebdesign">
+            <h2 id="lucerowebdesign">2004 – 2005</h2>
+            <div>
+              <h3>
+                <i className="bi bi-geo-alt-fill"></i> Fresno, CA
+              </h3>
+              <h3>
+                Lucero Web Design | <strong>Owner</strong>
+              </h3>
+              <ul>
+                <li>
+                  Owned/operated full service design & maintenance: nonprofit,
+                  higher education and small businesses
+                </li>
+                <li>Managed consultants</li>
+                <li>
+                  <strong>CSU, Fresno: PHP/MySQL</strong> phone directory, class
+                  requirements site
+                </li>
+              </ul>
+            </div>
+          </section>
+          <section aria-labelledby="csufresno">
+            <h2 id="csufresno">2001 – 2004</h2>
+            <div>
+              <h3>
+                <i className="bi bi-geo-alt-fill"></i> Fresno, CA
+              </h3>
+              <h3>
+                CSU, Fresno | <strong>Assistant Web Manager</strong>
+              </h3>
+              <ul>
+                <li>
+                  Design/manage/maintain integral university sites:{" "}
+                  <strong>Web accessibility guidelines, primary site</strong>,
+                  Fresno State News, University Journal,{" "}
+                  <strong>Phone Directory, Site Index</strong>, and directed
+                  contact page inquiries
+                </li>
+                <li>
+                  Trained students/faculty/administrators on policy, templates,
+                  and guidelines
+                </li>
+              </ul>
+            </div>
+          </section>
+        </Col>
+      </Row>
+    </>
   );
 };
 
