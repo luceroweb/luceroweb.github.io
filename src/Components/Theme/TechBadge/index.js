70e877d6bf866e53661c73f51f03ee9a09a77ad8
Juan Lucero <luceroweb@gmail.com>
2023-11-17 14:49:59 -0800

    feat(deprecated): show/hide deprecated proficiency

--- src/Components/Theme/TechBadge/index.js
+++ src/Components/Theme/TechBadge/index.js
@@ -1,10 +1,10 @@
 import { Badge } from "react-bootstrap";
 
-const TechBadge = ({ badge, isShort }) => {
-  return (
+const TechBadge = ({ badge, isShort, showDeprecated }) => {
+  return badge.deprecated && !showDeprecated ? null : (
     <span>
       <Badge className={badge.class}>
-        {isShort && badge.shortTitle ? badge.shortTitle : badge.title}
+        {isShort && badge.shortTitle ? badge.shortTitle : badge.title}{" "}
       </Badge>{" "}
     </span>
   );
